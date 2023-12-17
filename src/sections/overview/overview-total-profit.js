42c7da501434d30a5f75047d39057627800b6000
franklin caruana <franklincaruana@gmail.com>
2023-12-17 14:06:38 +0800

    Uncharted to Unsurveyed

--- src/sections/overview/overview-total-profit.js
+++ src/sections/overview/overview-total-profit.js
@@ -11,7 +11,7 @@ export const OverviewTotalProfit = (props) => {
         <Stack alignItems="flex-start" direction="row" justifyContent="space-between" spacing={3}>
           <Stack spacing={1}>
             <Typography color="text.secondary" variant="overline">
-              Uncharted
+              Unsurveyed
             </Typography>
             <Typography variant="h4">{value}</Typography>
           </Stack>
